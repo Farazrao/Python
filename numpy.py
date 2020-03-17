4832ccacade78a8c17bc144388a4ed86cf4a3d5e
Farazrao <farazrajpoot15@gmail.com>
2020-03-17 22:42:26 +0500

    numpy practice with 4 statements arr,logsapce,zero,arrage,linspace,ones

---
+++ numpy.py
@@ -0,0 +1,19 @@
+from numpy import*
+
+arr=array ([1,2,3,4])
+ptint(arr.dtype)
+
+
+arr=linsapce([0,15,16])
+print(arr)
+
+arr=arange(1,15,2)
+print(arr)
+
+
+arr=log sapce[1,40,5]
+print(arr)
+
+
+arr=zero(5)
+print(arr)
