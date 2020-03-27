fece37e751b7dfb909beefb4a9eb775da08d470c
Farazrao <farazrajpoot15@gmail.com>
2020-03-27 20:35:43 +0500

    recursion set the limit

---
+++ recursion.py
@@ -0,0 +1,12 @@
+import sys
+sys.setrecursionlimit(2000)
+# here we set the recursion limit
+print (sys.getrecursionlimit())
+# here we get the recursion
+i=0
+def greet ():
+    global i
+    i+=1
+    print("hello",+i)
+    greet()
+greet()
