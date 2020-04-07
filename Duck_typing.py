855510d3f4277d76fbd2354bb32e0e9953a01ecc
Farazrao <farazrajpoot15@gmail.com>
2020-04-07 12:10:44 +0200

    DUCK TYPING

---
+++ Duck_typing.py
@@ -0,0 +1,18 @@
+class pycharm:
+    def execute(self):
+        print("compiling")
+        print("running")
+class Myeditor:
+    def execute(self):
+        print("faraz")
+        print("rao")
+        print("compiling")
+        print("running")
+class laptop:
+    # ide= integrated development environment
+    def code(self,ide):
+        ide.execute()
+
+ide=Myeditor()
+lap1=laptop()
+lap1.code(ide)
